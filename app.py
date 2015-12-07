d50ee0e77d00ee729d8a20e8622de6a599d6e375
creativcoder <rsconceptx@gmail.com>
2015-12-07 14:39:45 +0530

    changes to db

--- app.py
+++ app.py
@@ -4,8 +4,33 @@ from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
+import requests
+import subprocess
 
 app = Flask(__name__)
+
+def update_db(db_file):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Referer': 'https://docs.google.com/spreadsheets/d/1HUL5jcpy5-nsw_b9TFPVhXsGkZ9NiiwfT0Wc5JG-jmY/edit?usp=sharing',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
+        }
+    resp = requests.get("""https://docs.google.com/spreadsheets/export?
+        format=csv&
+        id=1HUL5jcpy5-nsw_b9TFPVhXsGkZ9NiiwfT0Wc5JG-jmY&gid=444587373""", headers=headers)
+    if int(resp.status_code) == 200:
+        f = open(db_file+'.csv','w+')
+        f.write(resp.content)
+        f.close()
+    else:
+        print("Error fetching updated database")
+
+
+@app.route("/refresh")
+def refresh():
+    update_db('new_flood_stat')
+    return render_template("index.html")
 
 @app.route("/")
 def index():
@@ -27,24 +52,20 @@ FIELDS = {'Timestamp':True,
 
 connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
 collection = connection[DBS_NAME][COLLECTION_NAME]
-projects = collection.find(projection=FIELDS)
+reviews = collection.find(projection=FIELDS)
 
 @app.route("/floodstat/review")
 def floodstat_reivew():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS)
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
+    reviews = collection.find(projection=FIELDS)
+    json_reviews = []
+    for r in reviews:
+        json_reviews.append(r)
+    json_reviews = json.dumps(json_reviews, default=json_util.default)
     connection.close()
-    return json_projects
-
-
-# for project in projects:
-#     print project
-
+    return json_reviews
 
 if __name__ == "__main__":
+    update_db('flood_stat')
     app.run(host='0.0.0.0',port=5000,debug=True)
